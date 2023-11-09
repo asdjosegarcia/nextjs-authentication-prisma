a48b84d3ca013f6e48bc27f23e7e7bb99133f68f
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-09 18:23:04 -0300

    add login

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 //este código configura la autenticación usando NextAuth.js y el proveedor de credenciales. se puede configurarar dependiendo de las necesidades de autenticacion
 import NextAuth from "next-auth/next";
 import CredentialsProvider from 'next-auth/providers/credentials'; //proveedor de autenticación de credenciales, nos permite autenticar las credenciales 
-import db from "@/libs/db";
+import db from "@/libs/db";//importamos la base de datos    
 import bcrypt from 'bcrypt'
 
 
@@ -9,23 +9,24 @@ const authOptions = {//objeto donde almacenamos la configuracion para enviarsela
     providers: [ //arreglo que especifica los proveedores de autenticacion
         CredentialsProvider({//se utiliza para la autenticacion basada en credenciales, nombre, usuario y contraseña
             name: "Credentials", //
-            credentials: {//objeto que define los campos de credenciales requeridos para la autenticación, en este caso email, password
+            credentials: {//objeto que define los campos de credenciales requeridos para la autenticación, en este caso email, password, tambien podemos crear uno propio, pero de momento lo vamos a probar
                 email: { label: "Email", type: "text", placeholder: "jsmith" }, //estas propiedades generan el input de un login hubicado en //http://localhost:3000/api/auth/signin
                 password: { label: "Password", type: "password", placeholder: "*****" }
             },
-            async authorize(credentials, req) {//funcion que que autoriza/verifica a el usuario, recive el contenido de lo que escribamos en el input
+            async authorize(credentials, req) {//funcion que que autoriza/verifica a el usuario, recive el contenido de lo que escribamos en el input, y la informacionde la peticion
                 console.log(credentials)
                 const userFound =await db.user.findUnique({//almacena el usuario,await por por que sino nos manda cualquier cosa
                     where: {
                         email: credentials.email
                     }
                 })
-                if(!userFound)return null //si no se encuentra un usario como en ingresado en la db retorna null    
-                console.log('userFound',userFound)//mostramos el usuario que se encontro con el email
-                const matchPassword= await bcrypt.compare(credentials.password,userFound.password) //esto nos da un true o false,true si coinciden las contraseñas
-                if(!matchPassword) return null //si no coincide retorna null
+                if(!userFound) throw new Error('No user found') //si no se encuentra un usario como en ingresado en la db retorna null    
 
-                return{//si toda la autorizacion paso correcta
+                console.log('userFound',userFound)//mostramos el usuario que se encontro con el email
+                const matchPassword= await bcrypt.compare(credentials.password,userFound.password) //esto nos da un true o false,true si coinciden las contraseñas encriptadas
+                if(!matchPassword) throw new Error('Password wrong') //si no coincide retorna null(no hay usuario)
+
+                return{//si toda la autorizacion paso correcta, se envia esta informacion como cookie
                     id:userFound.id,
                     name:userFound.username,
                     email:userFound.email,
@@ -39,6 +40,6 @@ const authOptions = {//objeto donde almacenamos la configuracion para enviarsela
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST } //para tener anbas funciones manejadas por el mismo manejador de funcion?
-//exporta la variable handler 2 veces, 1 con GET y otra con POST
+export { handler as GET, handler as POST } //,para tener anbas funciones manejadas por el mismo manejador de funcion?
+//cuando llegue una peticion get y post se va a usar handler para manejar esta peticion
 //
--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
+import { useForm } from "react-hook-form";//para el manejo del formulario
+import { signIn } from 'next-auth/react'; //para el envio del formulario a la DB
+import { useRouter } from 'next/navigation'; //para la redirecciones
+
 
 const LoginPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm()//register para el manejo de los inputs, errors para el manejo de los errores, handleSubmit
+  const router = useRouter()//para las redirecciones
+  const [error,setEerror]=useState(null)//para almacenar el errror
+
+  const onSubmit = handleSubmit(async data => { //data es la informacion que tomamos del usuario
+    const res = await signIn('credentials', {//signIn es una funcion de next-auth para el logueo, ya sea con google,facebook etc, por defecto nos redirecciona
+      email: data.email,
+      password: data.password,
+      redirect: false,//para evitar el redireccionamiento al loguearse
+    })
+    if (res.error) {
+      // alert(res.error);
+      setEerror(res.error)
+    } else {
+      // console.log('enviando a /dashboard')
+      router.push('/dashboard') //si la autenticacion sale bien redireccionamos a el usuario a dashboard
+
+    }
+    console.log('res', res)
+  })
+
+
   return (
-    <div>LoginPage</div>
+    <div className='h-[calc(100vh-7rem)] flex justify-center items-center'>
+      <form onSubmit={onSubmit} className='w-1/4'>{/* una vez completado el formulario ejecuta la funcion onSubmit, que ejecuta handle submit */}
+        {error && (
+          <p className='bg-red-500 text-xs'>{error}</p>
+        )}
+        <h1 className="text-slate-400 font-bold text-4xl mb-4">Login</h1>
+        <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">Email</label>{/* texto superior */}
+        <input type="text" {...(register("email", { required: { value: true, message: 'Email is required' } }))} placeholder="yourUser123@email.com" className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full" />
+        {
+          errors.email && (//si error.email existe
+            <span className="text-red-500">{errors.email.message}</span>//se crea este span
+          )
+        }
+        <label htmlFor="password" className="text-slate-500 mb-2 block text-sm">Password</label>{/* texto superior */}
+        <input type="password" {...(register("password", { required: { value: true, message: 'Password is required' } }))} placeholder="******" className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full" />
+        {
+          errors.password && (//si error.password existe
+            <span className="text-red-500">{errors.password.message}</span>//se crea este span
+          )
+        }
+        <button className="w-full bg-blue-500 text-white p-3 rounded-lg">
+          Login
+        </button>
+      </form>
+    </div>
   )
 }
 
